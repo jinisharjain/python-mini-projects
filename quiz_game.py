1327969201a6688a32c9f098184c41957b8a6d26
Jinisha Jain <jinisharjain@gmail.com>
2026-03-14 14:25:04 +0530

    Add quiz game project

---
+++ quiz_game.py
@@ -0,0 +1,13 @@
+score = 0
+
+answer = input("Capital of India: ")
+
+if answer.lower() == "delhi":
+    score += 1
+
+answer = input("5 + 3 = ")
+
+if answer == "8":
+    score += 1
+
+print("Your score:", score)
